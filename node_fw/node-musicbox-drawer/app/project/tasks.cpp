79ded67a07e6b70f493dc22c8b2a5eb33cc1e8fd
cometu <come.on.ct@gmail.com>
2019-05-08 13:38:25 +0200

    Add drawer files

---
+++ node_fw/node-musicbox-drawer/app/project/tasks.cpp
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include <user_config.h>
+#include <SmingCore/SmingCore.h>
+
+#include "http_client.h"
+
+static Timer heartBeatTimer;
+
+uint32_t threshold = 15000;
+
+void nodeHeartBeatInit()
+{
+    heartBeatTimer.initializeMs(30 * 1000, sendHeartBeat);
+}
