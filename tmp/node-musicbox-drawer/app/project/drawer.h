52cf93c51a0a420ad16859f44534d36264a04a26
cometu <come.on.ct@gmail.com>
2019-06-04 08:39:58 +0200

    Rm old files

--- tmp/node-musicbox-drawer/app/project/drawer.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MUSICBOX_DRAWER__H__
-#define __MUSICBOX_DRAWER__H__
-
-void drawerInit(void);
-void nodeHeartBeatInit(void);
-
-#endif /* __MUSICBOX_DRAWER__H__ */
--- tmp/node-shrooms/app/project/shroom_task.h
+++
@@ -1,7 +0,0 @@
-#ifndef __NODE_EXAMPLE__H__
-#define __NODE_EXAMPLE__H__
-
-void shroomInit(void);
-void nodeHeartBeatInit(void);
-
-#endif /* __NODE_EXAMPLE__H__ */
