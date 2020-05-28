a2700d28dd6d46cf79294a701afad9d9436c0fd2
come <come.turrata@gmail.com>
2020-05-28 20:49:05 +0200

    Update component.mk

--- node_fw/node-musicbox/component.mk
+++ node_fw/node-musicbox/component.mk
@@ -1,6 +1,21 @@
-include $(PROJECT_PATH)/common.mk 
+mkfile_path := $(abspath $(lastword $(MAKEFILE_LIST)))
+mkfile_dir := $(dir $(mkfile_path))
+PROJECT_PATH =  $(mkfile_dir)/..
+
+include $(PROJECT_PATH)/common.mk
 
 ARDUINO_LIBRARIES := MCP23017
 
 NODE_HOSTNAME=\"musicbox\"
 COMPONENT_CFLAGS += -DNODE_HOSTNAME=$(NODE_HOSTNAME)
+
+SMING_ARCH = Esp8266
+SPI_MODE = qio
+SPI_SIZE = 4M
+COM_SPEED_ESPTOOL = 460800
+COM_PORT = /dev/ttyUSB0
+COM_OPTS = --raw
+COM_SPEED = 460800
+COM_SPEED_ESPTOOL = $(COM_SPEED)
+COM_SPEED_SERIAL = $(COM_SPEED)
+
