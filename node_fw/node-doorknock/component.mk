c109eeec2c6bffb7af45b5d538f4e04d3f6a5fe6
come <come.turrata@gmail.com>
2020-05-28 20:27:18 +0200

    Update component.mk

--- node_fw/node-doorknock/component.mk
+++ node_fw/node-doorknock/component.mk
@@ -1,5 +1,19 @@
+mkfile_path := $(abspath $(lastword $(MAKEFILE_LIST)))
+mkfile_dir := $(dir $(mkfile_path))
+PROJECT_PATH =  $(mkfile_dir)/..
+
 include $(PROJECT_PATH)/common.mk
 
-NODE_HOSTNAME=\"doorknock\"
 
+NODE_HOSTNAME=\"doorknock\"
 COMPONENT_CFLAGS += -DNODE_HOSTNAME=$(NODE_HOSTNAME)
+
+SMING_ARCH = Esp8266
+SPI_MODE = qio
+SPI_SIZE = 4M
+COM_SPEED_ESPTOOL = 460800
+COM_PORT = /dev/ttyUSB0
+COM_OPTS = --raw
+COM_SPEED = 460800
+COM_SPEED_ESPTOOL = $(COM_SPEED)
+COM_SPEED_SERIAL = $(COM_SPEED)
