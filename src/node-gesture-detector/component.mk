b6f8ac9dfe04f19ec4611669f046e94245bc07c7
Come Turrata <come@getluko.com>
2020-06-21 17:00:39 +0200

    Rename node hostnames to keep consistency

--- src/node-gesture-detector/component.mk
+++ src/node-gesture-detector/component.mk
@@ -4,7 +4,7 @@ PROJECT_PATH =  $(mkfile_dir)/..
 
 include $(PROJECT_PATH)/common.mk
 
-NODE_HOSTNAME=\"gesture_sensor\"
+NODE_HOSTNAME=\"gesturesensor\"
 COMPONENT_CXXFLAGS += -DNODE_HOSTNAME=$(NODE_HOSTNAME)
 
 SMING_ARCH = Esp8266
--- src/node-musicbox-drawer/component.mk
+++ src/node-musicbox-drawer/component.mk
@@ -4,5 +4,5 @@ PROJECT_PATH =  $(mkfile_dir)/..
 
 include $(PROJECT_PATH)/common.mk 
 
-NODE_HOSTNAME=\"musicbox-drawer\"
+NODE_HOSTNAME=\"musicboxdrawer\"
 COMPONENT_CXXFLAGS += -DNODE_HOSTNAME=$(NODE_HOSTNAME)
